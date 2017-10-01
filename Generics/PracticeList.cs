cb1002d92d741c4ffce0ac813c9a0fa46169efaf
Firefly4266 <gaines.johnp@gmail.com>
2017-10-01 13:43:35 -0700

    created generic list class with constructor

---
+++ Generics/PracticeList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    //creat a generic class
+    public class PracticeList<T>
+    {
+        //create an array of generic type "T"
+        private T[] items;
+
+        //add a constructor to our generic class
+        public PracticeList()
+        {
+            items = new T[0];
+        }
+    }
+}
